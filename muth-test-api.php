b9b101fecf5c17748b495eb1d8b17e1adf6d258b
Helios Tech <155942467+heliostechlabs@users.noreply.github.com>
2024-01-22 15:37:40 +0545

    Update muth-test-api.php

--- muth-test-api.php
+++ muth-test-api.php
@@ -2,10 +2,19 @@
 
 $ch = curl_init();
 
+
+
+
+$mobile = '9418628877';
+$first_name = 'Ankush';
+$last_name = 'Monjan';
+$email = 'ankush.thakur1025@gmail.com';
+$pin = '160055';
+
 curl_setopt($ch, CURLOPT_URL, 'https://middleware.muthoot.org:1880/Spice');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, '{"mobile": "9689009872","firstName" : "anku","lastdName" : "eighteenjan","email" : "ankush@heliostechlabs.com","pin" : 174101,"utm" : "SpiceMoney","owner" : 1,"medium" : 246,"source" : 14,"leadcreatedbytype" : 3,"preferredChannel" : 2,"priority" : 5}');
+curl_setopt($ch, CURLOPT_POSTFIELDS, '{"mobile": "'.$mobile.'","firstName" : "'.$first_name.'","lastdName" : "'.$last_name.'","email" : "'.$email.'","pin" : '.$pin.',"utm" : "SpiceMoney","owner" : 1,"medium" : 246,"source" : 14,"leadcreatedbytype" : 3,"preferredChannel" : 2,"priority" : 5}');
 
 $headers = array();
 $headers[] = 'Owner: SpiceMoney';
